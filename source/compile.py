1fcc783a90e1ee743f25dd211972d82d638d1794
vincentBenet <vincent.benet@outlook.fr>
2021-03-13 12:23:09 +0100

    ajust_compilation

--- source/compile.py
+++ source/compile.py
@@ -1,8 +1,4 @@
 import os
 import sys
-
-dir = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir)
-
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 os.system("cxfreeze -c time_reporting.py")
-os.rename(os.path.join(dir, "dist"), os.path.join(dir, "..", "..", "time_reportingApp"))
